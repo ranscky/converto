91491cf9fe360c66e6d449a3c13f261bc6d71ca4
ranscky <ransford711@gmail.com>
2025-08-15 11:17:49 +0000

    set up mongodb atlas

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3001;
 const cors = require('cors');
+const { MongoClient } = require('mongodb')
 
 app.use(cors());
-app.get('/api/test', (req, res) => {
-    res.json({ message: 'Welcome to the Converto backend!' });
+const uri = 'mongodb+srv://ransford711:GulOkeSAfKfFzbv5@convertodb.kjwxzyy.mongodb.net/?retryWrites=true&w=majority&appName=ConvertoDB'
+const client = new MongoClient(uri);
+
+app.get('/api/test', async(req, res) => {
+    try {
+        await client.connect();
+        const database = client.db('converto');
+        await database.collection('test').insertOne({ message: 'Hello from Converto!' });
+        res.json({ message: 'MongoDB connected!' });
+    } catch (error) {
+        res.json({ message: 'Error connecting to database' + error});
+    }
 });
 
 app.listen(port, () => {
